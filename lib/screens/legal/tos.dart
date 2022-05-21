9db5e2176586e9136979ffe81c4cd047d0e53ea4
Shirshajit Sen Gupta <shirshajit@gmail.com>
2022-05-22 05:20:54 +0800

    Login - Barebones

---
+++ lib/screens/legal/tos.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TermsOfService extends StatelessWidget {
+  const TermsOfService({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
