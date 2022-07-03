a5fe4ae196c5c4bb628a8f14ecc48f4e35e2af3e
Ishita <ishitamandal06@gmail.com>
2022-07-03 19:24:35 +0800

    app.json

--- lib/screens/search/find_by_building.dart
+++ lib/screens/search/find_by_building.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:building/components/app_bar.dart';
 
 class FindByBuilding extends StatefulWidget {
   const FindByBuilding({Key? key}) : super(key: key);
@@ -8,22 +9,45 @@ class FindByBuilding extends StatefulWidget {
 }
 
 class _FindByBuildingState extends State<FindByBuilding> {
-  late final TextEditingController _search;
-
-  @override
-  void initState() {
-    _search = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _search.dispose();
-    super.dispose();
-  }
+  List buildings = [
+    "School of Medicine",
+    "University Hall",
+    "COM",
+    "Engineering",
+    "Science",
+    "FASS",
+    "Biz",
+    "YIH",
+    "SDE",
+    "UTown",
+    "PGPR",
+    "I4",
+    "I3",
+    "CLB",
+    "USC",
+    "UHC",
+    "Museum",
+    "NUH"
+  ];
+  // late final TextEditingController _search;
+  //
+  // @override
+  // void initState() {
+  //   _search = TextEditingController();
+  //   super.initState();
+  // }
+  //
+  // @override
+  // void dispose() {
+  //   _search.dispose();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: ListView(children: [
+      screenAppBar("SEARCH BY BUILDING"),
+    ]));
   }
 }
